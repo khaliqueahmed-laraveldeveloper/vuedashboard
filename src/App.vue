410716b152eb52afecf5021d3f1b1e9a72820434
khalique <khaliqahmed1995@gmail.com>
2026-04-23 08:37:02 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import test from './components/test.vue'
+</script>
+
+<template>
+ <h3>Vue 3 + Vite + TypeScript + Tailwind CSS</h3>
+ <p>This is a simple vue with more details data </p>
+  <test></test>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/test.vue
@@ -0,0 +1,43 @@
+<template>
+<form v-on:submit.prevent="handleSubmit">
+    <label for="name">Name:</label>
+    <input type="text" id="name" v-model="name" required />
+    
+    <label for="email">Email:</label>
+    <input type="email" id="email" v-model="email" required />
+    
+    <button type="submit">Submit</button>
+
+</form>
+<ul>
+    <li v-for="(user, index) in users" :key="index">
+        {{ user.name }} - {{ user.email }}
+    </li>
+</ul>
+</template>
+<script >
+export default {
+    data() {
+        return {
+            name:'',
+            email:'',
+            users:[]
+        }
+    },
+    methods: {
+        handleSubmit(){
+        
+        let user={
+        name:this.name,
+        email:this.email
+        }
+
+        this.users.push(user);
+
+        this.name=null;
+        this.email=null;
+
+        }
+    },
+}
+</script>
